69bfc89d7b32130503bc40456efa72c3196c1035
MatiV <matias.valladares@udla.edu.ec>
2026-05-10 23:12:14 -0500

    Proyecto_Alternativa

--- funciones.h
+++ funciones.h
@@ -7,11 +7,11 @@
 int validateInts();
 float validateFloats();
 void readChain(char *chain, int n);
-int menu ( char chips[MAX_CHIPS][20] , int stockChips[MAX_CHIPS], int *buttons, int *metalParts, int productsCostMaterial [MAX_CHIPS][MAX_CHIPS], char products[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS],  float productsPrice [MAX_CHIPS]);
-void enterMaterials (char chips[MAX_CHIPS][20] , int stockChips[MAX_CHIPS], int *buttons, int *metalParts);
-void registerProducts (int productsCostMaterial[MAX_CHIPS][MAX_CHIPS], char products[MAX_CHIPS][20], char chips [MAX_CHIPS][20] , int productsChips[MAX_CHIPS][MAX_CHIPS],  float productsPrice [MAX_CHIPS]);
-void createAndSellProducts(char chips[MAX_CHIPS][20] , int stockChips[MAX_CHIPS], int *buttons, int *metalParts , int productsCostMaterial[MAX_CHIPS][MAX_CHIPS],  char products[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS]);
-void addMaterialStock (char chips[MAX_CHIPS][20], int *buttons, int *metalParts, int stockChips[MAX_CHIPS]);
+int menu(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int *buttons, int *metalParts, int productsCostMaterial [MAX_CHIPS][MAX_CHIPS], char products[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS]);
+void enterMaterials(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int *buttons, int *metalParts);
+void registerProducts(int productsCostMaterial[MAX_CHIPS][MAX_CHIPS], char products[MAX_CHIPS][20], char chips [MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS]);
+void createAndSellProducts(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int *buttons, int *metalParts , int productsCostMaterial[MAX_CHIPS][MAX_CHIPS], char products[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS]);
+void addMaterialStock(char chips[MAX_CHIPS][20], int *buttons, int *metalParts, int stockChips[MAX_CHIPS]);
 void changeProductsName(char products[MAX_CHIPS][20]);
 void changeProductsMaterialsNeed(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
 void deleteProduct(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
