ffac40e46199b0172a3fe5803cef5c7d612f0db8
MatiV <matias.valladares@udla.edu.ec>
2026-05-13 12:29:15 -0500

    Alternativa

--- funciones.h
+++ funciones.h
@@ -14,4 +14,6 @@ void createAndSellProducts(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS],
 void addMaterialStock(char chips[MAX_CHIPS][20], int *buttons, int *metalParts, int stockChips[MAX_CHIPS]);
 void changeProductsName(char products[MAX_CHIPS][20]);
 void changeProductsMaterialsNeed(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
-void deleteProduct(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
+void deleteProductandtheMaterialsitneeds(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
+void viewMaterialsStock(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int buttons, int metalParts);
+void viewProductsInfo(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
