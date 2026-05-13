662a1fcaf726073aad218d51f97fac625f1dc2b9
MatiValla95 <matias.valladares@udla.edu.ec>
2026-05-13 14:06:13 -0500

    Correction2

--- funciones.h
+++ funciones.h
@@ -15,5 +15,5 @@ void addMaterialStock(char chips[MAX_CHIPS][20], int *buttons, int *metalParts, 
 void changeProductsName(char products[MAX_CHIPS][20]);
 void changeProductsMaterialsNeed(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
 void deleteProductandtheMaterialsitneeds(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
-void viewMaterialsStock(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int buttons, int metalParts);
+void viewMaterialsStock(char chips[MAX_CHIPS][20], int stockChips[MAX_CHIPS], int *buttons, int *metalParts);
 void viewProductsInfo(char products[MAX_CHIPS][20], char chips[MAX_CHIPS][20], int productsChips[MAX_CHIPS][MAX_CHIPS], float productsPrice [MAX_CHIPS], int productsCostMaterial[MAX_CHIPS][MAX_CHIPS]);
